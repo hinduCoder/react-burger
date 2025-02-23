b88962aa531a57a19303e0230fdbbd30efd8fb37
Anton Gribov <gribov_an@astralnalog.ru>
2025-02-23 21:07:26 +0300

    fixed ingredients markup

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -4,15 +4,20 @@ import styles from './burger-ingredients.module.css'
 import { useState } from "react";
 const BurgerIngredients = () => {
     const [selectedIngredientType, setSelectedIngredientType] = useState('bun');
-    const tabs = [
+    const ingredientTypes = [
         { type: 'bun', label: 'Булки' },
         { type: 'sauce', label: 'Соусы' },
         { type: 'main', label: 'Начинки' },
-    ]
+    ];
+    
+    const ingredientTypeItemsMap = new Map();
+    ingredientTypes.forEach(ingredientType => ingredientTypeItemsMap.set(ingredientType.type, []));
+    data.forEach(item => ingredientTypeItemsMap.get(item.type).push(item));
+    
     return <section className={styles.ingredients_section}>
         <h1 className="text text_type_main-large" style={{textAlign: 'left'}}>Соберите бургер</h1>
         <nav className={styles.tabs}>
-            {tabs.map(tab => 
+            {ingredientTypes.map(tab => 
                 <Tab 
                     key={tab.type} 
                     active={selectedIngredientType === tab.type} 
@@ -21,20 +26,25 @@ const BurgerIngredients = () => {
                     {tab.label}
                 </Tab>)}
         </nav>
-        <ul className={styles.ingredient_grid}>
-            {data
-                .filter(ingredient => ingredient.type === selectedIngredientType)
-                .map(ingredient => 
-                    <li key={ingredient._id} className={styles.ingredient_card}>
-                        {ingredient.count && <Counter count={ingredient.count}/>}
-                        <img alt={ingredient.name} src={ingredient.image} className="ml-4 mr-4"/>
-                        <div>
-                            <span className="text text_type_digits-default">{ingredient.price} </span>
-                            <CurrencyIcon type="primary"/>
-                        </div>
-                        <div className="text text_type_main-default">{ingredient.name}</div>
-                    </li>)}
-        </ul>
+        <section className={styles.ingredients_list}>
+            {ingredientTypes.map(ingredientType => <>
+                <h2 style={{textAlign: 'left'}} className="text text_type_main-medium">{ingredientType.label}</h2>
+                <ul className={styles.ingredient_grid}>
+                    {data
+                        .map(ingredient =>
+                            <li key={ingredient._id} className={styles.ingredient_card}>
+                                {ingredient.count && <Counter count={ingredient.count}/>}
+                                <img alt={ingredient.name} src={ingredient.image} className="ml-4 mr-4"/>
+                                <div>
+                                    <span className="text text_type_digits-default">{ingredient.price} </span>
+                                    <CurrencyIcon type="primary"/>
+                                </div>
+                                <div className="text text_type_main-default">{ingredient.name}</div>
+                            </li>)}
+                </ul>
+        </>)}
+        </section>
+
     </section>
 }
 
