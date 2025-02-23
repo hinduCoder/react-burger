b1371fb8fc5e3c337dff99a4603ec1aee93e0cfa
Anton Gribov <gribov_an@astralnalog.ru>
2025-02-23 21:23:40 +0300

    navigation menu tags div -> a

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -4,21 +4,21 @@ import styles from './app-header.module.css';
 const AppHeader = () => {
     return <header className={styles.header + ' text text_color_inactive text_type_main-default'}>
         <nav className={styles.menu}>
-            <div className="pl-5 pr-5 pt-4 pb-4 mt-4 mb-4">
+            <a className="pl-5 pr-5 pt-4 pb-4 mt-4 mb-4">
                 <BurgerIcon type="primary"/>
                 <span className="text_color_primary">Конструктор</span>
-            </div>
-            <div className="pl-5 pr-5 pt-4 pb-4 mt-4 mb-4">
+            </a>
+            <a className="pl-5 pr-5 pt-4 pb-4 mt-4 mb-4">
                 <ListIcon type="secondary"/>
                 <span>Лента заказов</span>
-            </div>
+            </a>
         </nav>
         <Logo className={styles.logo}/>
         <nav className={styles.menu}>
-            <div className="pl-5 pr-5 pt-4 pb-4 mt-4 mb-4">
+            <a className="pl-5 pr-5 pt-4 pb-4 mt-4 mb-4">
                 <ProfileIcon type="secondary" />
                 <span >Личный кабинет</span>
-            </div>
+            </a>
         </nav>
     </header>
 }
