8071ff261db9d0661171022890168d41172375ba
Anton Gribov <gribov_an@astralnalog.ru>
2025-02-24 00:20:49 +0300

    autoscroll on tab click

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,12 +1,13 @@
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components'
 import styles from './burger-ingredients.module.css'
-import { useState } from "react";
+import { useRef, useState } from "react";
 import dataType from '../../utils/data.proptypes'
 import PropTypes from "prop-types";
 import IngredientCard from "../ingredient-card/ingredient-card";
 
 const BurgerIngredients = ({ ingredients }) => {
     const [selectedIngredientType, setSelectedIngredientType] = useState('bun');
+    const ingredientTypeHeaderRefs = useRef({});
     const ingredientTypes = [
         { type: 'bun', label: 'Булки' },
         { type: 'sauce', label: 'Соусы' },
@@ -18,20 +19,23 @@ const BurgerIngredients = ({ ingredients }) => {
     ingredients.forEach(item => ingredientTypeItemsMap.get(item.type).push(item));
     
     return <section className={styles.ingredients_section}>
-        <h1 className="text text_type_main-large" style={{textAlign: 'left'}}>Соберите бургер</h1>
+        <h1 className="text text_type_main-large" style={{textAlign: 'left'}} >Соберите бургер</h1>
         <nav className={styles.tabs}>
             {ingredientTypes.map(tab => 
                 <Tab 
                     key={tab.type} 
                     active={selectedIngredientType === tab.type} 
                     value={tab.type} 
-                    onClick={() => setSelectedIngredientType(tab.type)}>
+                    onClick={() =>  {
+                        setSelectedIngredientType(tab.type);
+                        ingredientTypeHeaderRefs.current[tab.type].scrollIntoView({behavior: "smooth"});
+                    }}>
                     {tab.label}
                 </Tab>)}
         </nav>
         <section className={styles.ingredients_list}>
             {ingredientTypes.map(ingredientType => <>
-                <h2 style={{textAlign: 'left'}} className="text text_type_main-medium">{ingredientType.label}</h2>
+                <h2 style={{textAlign: 'left'}} className="text text_type_main-medium" ref={element => ingredientTypeHeaderRefs.current[ingredientType.type] = element}>{ingredientType.label}</h2>
                 <ul className={styles.ingredient_grid}>
                     {ingredients
                         .map(ingredient =>
