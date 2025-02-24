1b272025e94ba8c4c2129b716f40687f3ea6fe89
Anton Gribov <gribov_an@astralnalog.ru>
2025-02-24 20:06:28 +0300

    reformat

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -2,25 +2,26 @@ import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-dev
 import styles from './app-header.module.css';
 
 const AppHeader = () => {
-    return <header className={styles.header + ' text text_color_inactive text_type_main-default'}>
-        <nav className={styles.menu}>
-            <a className="pl-5 pr-5 pt-4 pb-4 mt-4 mb-4">
-                <BurgerIcon type="primary"/>
-                <span className="text_color_primary">Конструктор</span>
-            </a>
-            <a className="pl-5 pr-5 pt-4 pb-4 mt-4 mb-4">
-                <ListIcon type="secondary"/>
-                <span>Лента заказов</span>
-            </a>
-        </nav>
-        <Logo className={styles.logo}/>
-        <nav className={styles.menu}>
-            <a className="pl-5 pr-5 pt-4 pb-4 mt-4 mb-4">
-                <ProfileIcon type="secondary" />
-                <span >Личный кабинет</span>
-            </a>
-        </nav>
-    </header>
+    return (
+        <header className={styles.header + ' text text_color_inactive text_type_main-default'}>
+            <nav className={styles.menu}>
+                <a className="pl-5 pr-5 pt-4 pb-4 mt-4 mb-4">
+                    <BurgerIcon type="primary"/>
+                    <span className="text_color_primary">Конструктор</span>
+                </a>
+                <a className="pl-5 pr-5 pt-4 pb-4 mt-4 mb-4">
+                    <ListIcon type="secondary"/>
+                    <span>Лента заказов</span>
+                </a>
+            </nav>
+            <Logo className={styles.logo}/>
+            <nav className={styles.menu}>
+                <a className="pl-5 pr-5 pt-4 pb-4 mt-4 mb-4">
+                    <ProfileIcon type="secondary" />
+                    <span >Личный кабинет</span>
+                </a>
+            </nav>
+        </header>)
 }
 
 
