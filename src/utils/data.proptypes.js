ddc9e0ec755ec66e3181f0e58110388d27c6409b
Anton Gribov <gribov_an@astralnalog.ru>
2025-02-24 10:56:25 +0300

    Fixed prop types

--- src/utils/data.proptypes.js
+++ src/utils/data.proptypes.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 export default PropTypes.shape({
     _id: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
-    type: PropTypes.oneOf('bun', 'main', 'sauce').isRequired,
+    type: PropTypes.oneOf(['bun', 'main', 'sauce']).isRequired,
     price: PropTypes.number.isRequired,
     image: PropTypes.string.isRequired,
     image_mobile: PropTypes.string.isRequired,
