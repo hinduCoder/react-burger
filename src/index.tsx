961e538081f5b8fa8b0c1f800074924907bb0662
Anton Gribov <gribov_an@astralnalog.ru>
2025-03-04 19:25:43 +0300

    react-dnd connect

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import App from './components/app/app';
 import reportWebVitals from './reportWebVitals';
 import store from "./services/store";
 import { Provider } from "react-redux";
+import {DndProvider} from "react-dnd";
+import {HTML5Backend} from "react-dnd-html5-backend";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,8 +14,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
       <Provider store={store}>
-
-        <App />
+        <DndProvider backend={HTML5Backend}>
+            <App />
+        </DndProvider>
       </Provider>
   </React.StrictMode>
 );
