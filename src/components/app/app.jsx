6742074b55c07c476e99610b76e12669453856cc
Anton Gribov <gribov_an@astralnalog.ru>
2025-03-07 12:00:24 +0300

    installed prettier, burger-constructor reformat

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,28 +1,27 @@
 import React, { useEffect } from 'react';
 import styles from './app.module.css';
-import AppHeader from "../app-header/app-header";
-import BurgerIngredients from "../burger-ingredients/burger-ingredients";
-import BurgerConstructor from "../burger-constructor/burger-constructor";
-import { useDispatch, useSelector } from "react-redux";
-import { loadData } from "../../services/ingredients";
-
+import AppHeader from '../app-header/app-header';
+import BurgerIngredients from '../burger-ingredients/burger-ingredients';
+import BurgerConstructor from '../burger-constructor/burger-constructor';
+import { useDispatch } from 'react-redux';
+import { loadData } from '../../services/ingredients';
 
 function App() {
     const dispatch = useDispatch();
-    
+
     useEffect(() => {
         dispatch(loadData());
-    }, [])
-    
+    }, [dispatch]);
+
     return (
         <div className={styles.app}>
             <AppHeader />
             <main className={styles.main}>
-                <BurgerIngredients  />
+                <BurgerIngredients />
                 <BurgerConstructor />
             </main>
         </div>
-  );
+    );
 }
 
 export default App;
--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,73 +1,108 @@
-import { ConstructorElement, CurrencyIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components'
-import styles from './burger-constructor.module.css'
-import Modal from "../modal/modal";
-import OrderDetails from "../order-details/order-details";
-import { useDispatch, useSelector } from "react-redux";
-import { useDrop } from "react-dnd";
-import { addFilling, setBun } from "../../services/burger-constructor";
-import { closeOrderInfo, makeOrder } from "../../services/order";
-import BurgerConstructorItem from "../burger-constructor-item/burger-constructor-item";
-import { decrementCount, incrementCount } from "../../services/ingredients";
+import {
+    ConstructorElement,
+    CurrencyIcon,
+    Button
+} from '@ya.praktikum/react-developer-burger-ui-components';
+import styles from './burger-constructor.module.css';
+import Modal from '../modal/modal';
+import OrderDetails from '../order-details/order-details';
+import { useDispatch, useSelector } from 'react-redux';
+import { useDrop } from 'react-dnd';
+import { addFilling, setBun } from '../../services/burger-constructor';
+import { closeOrderInfo, makeOrder } from '../../services/order';
+import BurgerConstructorItem from '../burger-constructor-item/burger-constructor-item';
+import { decrementCount, incrementCount } from '../../services/ingredients';
 
 const BurgerConstructor = () => {
     const { bun, fillings } = useSelector(store => store.burgerConstructor);
-    const { showOrderInfo, number: orderNumber } = useSelector(store => store.order);
-    
+    const { showOrderInfo } = useSelector(store => store.order);
+
     const dispatch = useDispatch();
-    
+
     const [, dropTarget] = useDrop({
         accept: 'ingredient',
-        
+
         drop({ ingredient }) {
             if (ingredient.type === 'bun') {
                 if (bun) {
-                    dispatch(decrementCount(bun._id))
+                    dispatch(decrementCount(bun._id));
                 }
                 dispatch(setBun(ingredient));
             } else {
                 dispatch(addFilling(ingredient));
             }
-            dispatch(incrementCount(ingredient._id))
+            dispatch(incrementCount(ingredient._id));
         }
-    })
-    const sum = (bun?.price ?? 0)*2 + fillings.reduce((result, current) => result + current.price, 0);
-    
+    });
+    const sum =
+        (bun?.price ?? 0) * 2 +
+        fillings.reduce((result, current) => result + current.price, 0);
+
     return (
         <section ref={dropTarget} className={styles.burger_constructor}>
             <div>
-                {bun && <ConstructorElement
-                    extraClass="ml-8"
-                    text={`${bun.name} (верх)`}
-                    thumbnail={bun.image_mobile}
-                    price={bun.price}
-                    type="top"
-                    isLocked={true}
-                />}
+                {bun && (
+                    <ConstructorElement
+                        extraClass="ml-8"
+                        text={`${bun.name} (верх)`}
+                        thumbnail={bun.image_mobile}
+                        price={bun.price}
+                        type="top"
+                        isLocked={true}
+                    />
+                )}
             </div>
             <ul className={styles.list}>
-                {fillings.map(item => <li key={item.localId}>
-                    <BurgerConstructorItem item={item} />
-                </li>)}
+                {fillings.map(item => (
+                    <li key={item.localId}>
+                        <BurgerConstructorItem item={item} />
+                    </li>
+                ))}
             </ul>
             <div>
-                {bun && <ConstructorElement
-                    extraClass="ml-8"
-                    text={`${bun.name} (низ)`}
-                    thumbnail={bun.image_mobile}
-                    price={bun.price}
-                    type="bottom"
-                    isLocked={true}
-                />}
+                {bun && (
+                    <ConstructorElement
+                        extraClass="ml-8"
+                        text={`${bun.name} (низ)`}
+                        thumbnail={bun.image_mobile}
+                        price={bun.price}
+                        type="bottom"
+                        isLocked={true}
+                    />
+                )}
             </div>
             <div className={styles.total}>
-                <span className="text text_type_digits-medium">{sum} <CurrencyIcon type="primary" className={styles.total_currency_icon}/>
+                <span className="text text_type_digits-medium">
+                    {sum}{' '}
+                    <CurrencyIcon
+                        type="primary"
+                        className={styles.total_currency_icon}
+                    />
                 </span>
-                <Button disabled={!bun} htmlType="button" type="primary" size="large" extraClass="ml-10" onClick={() => dispatch(makeOrder([bun._id, ...fillings.map(f => f._id)]))}>Оформить заказ</Button>
+                <Button
+                    disabled={!bun}
+                    htmlType="button"
+                    type="primary"
+                    size="large"
+                    extraClass="ml-10"
+                    onClick={() =>
+                        dispatch(
+                            makeOrder([bun._id, ...fillings.map(f => f._id)])
+                        )
+                    }>
+                    Оформить заказ
+                </Button>
             </div>
-            {showOrderInfo && <Modal onClose={() => { dispatch(closeOrderInfo())}}>
-                <OrderDetails />
-            </Modal>}
-        </section>)
-}
+            {showOrderInfo && (
+                <Modal
+                    onClose={() => {
+                        dispatch(closeOrderInfo());
+                    }}>
+                    <OrderDetails />
+                </Modal>
+            )}
+        </section>
+    );
+};
 
 export default BurgerConstructor;
