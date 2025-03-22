4d4104eccae9465921065e3f1e244f3e56ea1793
Anton Gribov <gribov_an@astralnalog.ru>
2025-03-23 02:58:18 +0300

    ingredients loading moved to main page

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,17 +1,8 @@
-import React, { useEffect } from 'react';
 import styles from './app.module.css';
 import AppHeader from '../app-header/app-header';
-import { useDispatch } from 'react-redux';
-import { loadData } from '../../services/ingredients';
 import { Outlet } from 'react-router-dom';
 
 function App() {
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(loadData());
-    }, [dispatch]);
-
     return (
         <div className={styles.app}>
             <AppHeader />
--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -1,8 +1,16 @@
 import BurgerIngredients from '../../components/burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../../components/burger-constructor/burger-constructor';
 import styles from './main.module.css';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { loadData } from '../../services/ingredients';
 
 const MainPage = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(loadData());
+    }, [dispatch]);
     return (
         <main className={styles.main}>
             <BurgerIngredients />
